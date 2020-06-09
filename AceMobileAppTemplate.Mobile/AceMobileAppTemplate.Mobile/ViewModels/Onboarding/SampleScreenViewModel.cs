18f22c7a8ba5f962a15cf597a451dcbc9effcf6d
Cody Gordon <codyjg10@gmail.com>
2020-06-09 05:58:04 -0400

    Added onboarding

---
+++ AceMobileAppTemplate.Mobile/AceMobileAppTemplate.Mobile/ViewModels/Onboarding/SampleScreenViewModel.cs
@@ -0,0 +1,38 @@
+﻿using AceMobileAppTemplate.Mobile.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace AceMobileAppTemplate.Mobile.ViewModels.Onboarding
+{
+    public class SampleScreenViewModel : OnboardingViewModelBase
+    {
+        public SampleScreenViewModel() : base() { }
+
+        protected override void InitializeOnBoarding()
+        {
+            Items = new ObservableCollection<OnboardingOverviewModel>
+            {
+                new OnboardingOverviewModel
+                {
+                    Title = "Welcome to \n My App!",
+                    Content = "This is my app's description",
+                    ImageUrl = "my_logo.png"
+                },
+                new OnboardingOverviewModel
+                {
+                    Title = "A cool feature",
+                    Content = "this is the cool feature's description",
+                    ImageUrl = "onboarding_image.png"
+                },
+                new OnboardingOverviewModel
+                {
+                    Title = "Another Feature",
+                    Content = "Here's another feature",
+                    ImageUrl = "another_image.png"
+                }
+            };
+        }
+    }
+}
